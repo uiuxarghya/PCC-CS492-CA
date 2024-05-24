675c40b6d896d8b7ae11411bfe560045acbdd75a
Arghya Ghosh <arghyaghosh997logos@gmail.com>
2024-05-24 13:17:12 +0000

    feat: add NAND gate

---
+++ Basic Gates/nand_test.vhd
@@ -0,0 +1,88 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   12:05:01 01/30/2024
+-- Design Name:   
+-- Module Name:   /home/student/Desktop/arghya/assignment_1.3/nand_gate/nand_test.vhd
+-- Project Name:  nand_gate
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: nand_rtl
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY nand_test IS
+END nand_test;
+ 
+ARCHITECTURE behavior OF nand_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT nand_rtl
+    PORT(
+         a : IN  std_logic;
+         b : IN  std_logic;
+         c : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal a : std_logic := '0';
+   signal b : std_logic := '0';
+
+ 	--Outputs
+   signal c : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+  
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: nand_rtl PORT MAP (
+          a => a,
+          b => b,
+          c => c
+        );
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+     
+	   a<='0';
+		b<='0';
+		wait for 1 ps;
+		a<='0';
+		b<='1';
+		wait for 1 ps;
+		a<='1';
+		b<='0';
+		wait for 1 ps;
+		a<='1';
+		b<='1';
+		wait for 1 ps;
+	  
+   end process;
+
+END;
